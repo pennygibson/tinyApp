80977175574b4328881dde0553d5ec3a95bb50b1
pennygibson <cenneboulet@yahoo.ca>
2017-08-02 19:44:01 +0000

    initial commit before cookies

---
+++ express_server.js
@@ -0,0 +1,86 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; //
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString(){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * 6));
+
+  return text;
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) =>{
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b> World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) =>{
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect("urls/" + shortURL);
+});
+
+app.post("/urls/:id/delete", (req, res) =>{
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls")
+});
+
+app.post("/urls/:id/update",(req, res) =>{
+  urlDatabase[req.params.id] = req.body.longURL;
+
+  res.redirect("/urls")
+})
+
+app.get("/u/:shortURL", (req, res) => {
+   let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
+
+
+
+
